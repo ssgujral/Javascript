aeffbde73b91306a54ee478e26e3ca7cc3da550a
samgujral <71089547+samgujral@users.noreply.github.com>
2022-02-27 13:37:47 +0100

    Bank Tech Test -- Class Created & Date Input Accepted

--- Bank_tech_test.js
+++ Bank_tech_test.js
@@ -1,77 +1,69 @@
-let balance = 0;
-let ledger = [];
 
-function getBalance(){
-  return balance;
-}
+  function get_date(date) 
+  {
+    var date_parts = date.split('/');
+    var day = date_parts[0];
+    var month = date_parts[1]-1;
+    var year = date_parts[2];
+    return new Date(year, month, day, 0, 0).toLocaleDateString
+    ('en-gb',
+      {
+        year: 'numeric',
+        month: 'numeric',
+        day: 'numeric',
+      }
+    );
+  }
 
-function increment(amount) {
-  balance = balance + amount;
-}
-
-
-function deposit(amount, date)
+class Account 
 {
+  constructor(balance, ledger)
+  {  
+    this.balance = balance;
+    this.ledger = ledger;
+    balance = 0;
+    ledger = []; 
+  }
 
-  var date_parts = date.split('/');
-  var day = date_parts[0];
-  var month = date_parts[1]-1;
-  var year = date_parts[2];
+  increment(amount) 
+  {
+    this.balance += amount;
+    return null;
+  }
 
-  var date1 = new Date(year, month, day, 0, 0).toLocaleDateString
-  ('en-gb',
-    {
-      year: 'numeric',
-      month: 'numeric',
-      day: 'numeric',
-    }
-  );
+  deposit(amount, date)
+  {
+    var date1 = get_date(date)
+    this.increment(amount);
+    var new_deposit_array = [date1, amount, 0, this.balance];
+    var formatted_deposit = new_deposit_array.join(' || ');
+    this.ledger.push([formatted_deposit]);
+    return null;
+  }
 
-  increment(amount);
-  new_deposit_array = [date1, amount, 0, getBalance()];
-  formatted_deposit = new_deposit_array.join(' || ');
-  ledger.push([formatted_deposit]);
-}
+  withdraw(amount, date)
+  {
+    var date2 = get_date(date);
+    this.increment(-amount); 
+    var new_withdrawal_array = [date2, 0, amount, this.balance];
+    var formatted_withdrawal = new_withdrawal_array.join(' || ');
+    this.ledger.push([formatted_withdrawal]);
+    return null;
+  }
 
-function withdraw(amount, date)
-{
-
-  var date_parts2 = date.split('/');
-  var day = date_parts2[0];
-  var month = date_parts2[1]-1;
-  var year = date_parts2[2];
-
-  var date2 = new Date(year, month, day, 0, 0).toLocaleDateString
-  ('en-gb',
-    {
-      year: 'numeric',
-      month: 'numeric',
-      day: 'numeric',
-    }
-  );
-
-  increment(-amount); 
-  new_withdrawal_array = [date2, 0, amount, getBalance()];
-  formatted_withdrawal = new_withdrawal_array.join(' || ');
-  ledger.push([formatted_withdrawal]);
-}
-
-function print_statement()
-{
-  console.log("date || credit || debit || balance");
-  const formatted_reversed_ledger = ledger.reverse().join("\r\n");
-  console.log(formatted_reversed_ledger);
+  print_statement()
+  {
+    console.log("date || credit || debit || balance");
+    const formatted_reversed_ledger = this.ledger.reverse().join("\r\n");
+    console.log(formatted_reversed_ledger);
+    return null;
+  }
 
 }
 
+let Account1 = new Account(0, []);
 
-
-
-
-
-
-deposit(1000, '10/01/2023');
-deposit(2000, '13/01/2023');
-withdraw(500, '14/01/2023');
-deposit(100000, '01/12/2023');
-print_statement();
+Account1.deposit(1000, '10/01/2023');
+Account1.deposit(2000, '13/01/2023');
+Account1.withdraw(500, '14/01/2023');
+Account1.print_statement();
